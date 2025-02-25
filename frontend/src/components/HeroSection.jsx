ff7aab82f07264f34e870417a1ec92fd4d20fa06
Fahad <akash.technobits@gmail.com>
2025-02-25 18:07:19 +0530

    update hero section

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -21,7 +21,7 @@ export const HeroSection = () => {
             <div className='flex flex-col gap-5 my-10'>
                 <span className='mx-auto px-4 py-2 rounded-full bg-gray-100 text-[#F83002] font-medium'>No. 1 Job Hunt Website</span>
                 <h1 className='text-5xl font-bold'>Search, Apply & <br /> Get Your <span className='text-[#6A38C2]'>Dream Jobs</span></h1>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur in sit dicta consequatur alias?</p>
+                <p>"India’s Leading Job Hunt Platform – Empowering You to Land Your Dream Job Faster. Discover Thousands of Opportunities Across Industries and Start Your Career Journey Today!"</p>
                 <div className='flex w-[30%] shadow-lg border border-gray-200 pl-3 rounded-full items-center gap-4 mx-auto'>
                     <input type="text"
                         placeholder='Find your dream jobs'
